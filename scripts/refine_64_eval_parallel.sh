efda9db43f6915f7be9e26dbde177eff22e1cec0
kzhangm02 <kevin0.0zhang@gmail.com>
2023-10-28 03:48:58 +0000

    update scripts

--- scripts/refine_64_eval_parallel.sh
+++ scripts/refine_64_eval_parallel.sh
@@ -7,9 +7,9 @@ gpu3=$4
 
 echo "===== Evaluating refine-64 model on: $dataset (gpu ids: $gpu1, $gpu2, $gpu3) ====="
 
-screen -S eval-"$dataset"-64_1 -dm bash -c "CUDA_VISIBLE_DEVICES="$gpu1" python ../eval.py ../configs/"$dataset"/refine64/config1.yaml ../logs/"$dataset"_encoder-decoder-64_1/lightning_logs/checkpoints ./logs_"$dataset"_refine-64_1/lightning_logs/checkpoints eval-eval; \
+screen -S eval-"$dataset"-64_1 -dm bash -c "CUDA_VISIBLE_DEVICES="$gpu1" python ../eval.py ../configs/"$dataset"/refine64/config1.yaml ../logs/"$dataset"_encoder-decoder-64_1/lightning_logs/checkpoints ../logs/"$dataset"_refine-64_1/lightning_logs/checkpoints eval-eval; \
                                             exec sh";
-screen -S eval-"$dataset"-64_2 -dm bash -c "CUDA_VISIBLE_DEVICES="$gpu2" python ../eval.py ../configs/"$dataset"/refine64/config2.yaml ../logs/"$dataset"_encoder-decoder-64_2/lightning_logs/checkpoints ./logs_"$dataset"_refine-64_2/lightning_logs/checkpoints eval-eval; \
+screen -S eval-"$dataset"-64_2 -dm bash -c "CUDA_VISIBLE_DEVICES="$gpu2" python ../eval.py ../configs/"$dataset"/refine64/config2.yaml ../logs/"$dataset"_encoder-decoder-64_2/lightning_logs/checkpoints ../logs/"$dataset"_refine-64_2/lightning_logs/checkpoints eval-eval; \
                                             exec sh";
-screen -S eval-"$dataset"-64_3 -dm bash -c "CUDA_VISIBLE_DEVICES="$gpu3" python ../eval.py ../configs/"$dataset"/refine64/config3.yaml ../logs/"$dataset"_encoder-decoder-64_3/lightning_logs/checkpoints ./logs_"$dataset"_refine-64_3/lightning_logs/checkpoints eval-eval; \
+screen -S eval-"$dataset"-64_3 -dm bash -c "CUDA_VISIBLE_DEVICES="$gpu3" python ../eval.py ../configs/"$dataset"/refine64/config3.yaml ../logs/"$dataset"_encoder-decoder-64_3/lightning_logs/checkpoints ../logs/"$dataset"_refine-64_3/lightning_logs/checkpoints eval-eval; \
                                             exec sh";
