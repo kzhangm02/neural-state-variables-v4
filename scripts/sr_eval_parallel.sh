bc1e14972099bb0eb7ebd34fe4354935c6db4033
kzhangm02 <kevin0.0zhang@gmail.com>
2023-10-31 16:56:33 +0000

    update scripts

--- scripts/sr_eval_parallel.sh
+++ scripts/sr_eval_parallel.sh
@@ -4,9 +4,9 @@ dataset=$1
 
 echo "===== Evaluating symbolic regression model on: $dataset ====="
 
-screen -S eval-"$dataset"-sr_1 -dm bash -c "python ../eval.py ../configs/"$dataset"/regression/config1.yaml ./logs_"$dataset"_refine-64_1/symbolic_regression/model.pkl NA eval-sr-train; \
+screen -S eval-"$dataset"-sr_1 -dm bash -c "python ../eval.py ../configs/"$dataset"/regression/config1.yaml ../logs/"$dataset"_refine-64_1/symbolic_regression/model.pkl NA eval-sr-train; \
                                             exec sh";
-screen -S eval-"$dataset"-sr_2 -dm bash -c "python ../eval.py ../configs/"$dataset"/regression/config2.yaml ./logs_"$dataset"_refine-64_2/symbolic_regression/model.pkl NA eval-sr-train; \
+screen -S eval-"$dataset"-sr_2 -dm bash -c "python ../eval.py ../configs/"$dataset"/regression/config2.yaml ../logs/"$dataset"_refine-64_2/symbolic_regression/model.pkl NA eval-sr-train; \
                                             exec sh";
-screen -S eval-"$dataset"-sr_3 -dm bash -c "python ../eval.py ../configs/"$dataset"/regression/config3.yaml ./logs_"$dataset"_refine-64_3/symbolic_regression/model.pkl NA eval-sr-train; \
+screen -S eval-"$dataset"-sr_3 -dm bash -c "python ../eval.py ../configs/"$dataset"/regression/config3.yaml ../logs/"$dataset"_refine-64_3/symbolic_regression/model.pkl NA eval-sr-train; \
                                             exec sh";
--- scripts/sr_eval_single.sh
+++ scripts/sr_eval_single.sh
@@ -5,5 +5,5 @@ seed=$2
 
 echo "===== Evaluating symbolic regression model on: $dataset ====="
 
-screen -S eval-"$dataset"-sr_"$seed" -dm bash -c "python ../eval.py ../configs/"$dataset"/regression/config"$seed".yaml ./logs_"$dataset"_refine-64_"$seed"/symbolic_regression/model.pkl NA eval-sr-train; \
+screen -S eval-"$dataset"-sr_"$seed" -dm bash -c "python ../eval.py ../configs/"$dataset"/regression/config"$seed".yaml ../logs/"$dataset"_refine-64_"$seed"/symbolic_regression/model.pkl NA eval-sr-train; \
                                                   exec sh";
